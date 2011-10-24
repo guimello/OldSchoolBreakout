4bbea3e10cd4eca28c3bcaa7d9187382adf5367f
guimello <guilhermesilvamello@gmail.com>
2011-10-24 20:51:06 -0200

    Initial commit

---
+++ Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  OldSchoolBreakout
+//
+//  Created by Guilherme da Silva Mello on 10/24/11.
+//  Copyright Guimello Tecnologia 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OldSchoolBreakout
+//
+//  Created by Guilherme da Silva Mello on 10/24/11.
+//  Copyright Guimello Tecnologia 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"OldSchoolBreakoutAppDelegate");
+	[pool release];
+	return retVal;
+}
